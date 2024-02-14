51f403b6c28b833d6d2862ec8cb01219163e9152
Nathan M.S <113636678+timid-angel@users.noreply.github.com>
2024-02-15 00:16:38 +0300

    Add solution for Rearrange Array Elements by Sign

--- src/leetcode/leetcode/rearrange-array-elements-by-sign.py
+++ src/leetcode/leetcode/rearrange-array-elements-by-sign.py
@@ -1,15 +1,17 @@
 class Solution:
-    def rearrangeArray(self, nums: List[int]) -> List[int]:
-        def getSign(num):
-            return -1 if num < 0 else 1
-        nums.sort(key=getSign)
-        
+    def rearrangeArray(self, nums: List[int]) -> List[int]:    
         newArr = [0] * len(nums)
         mid = len(nums) // 2
-        j = 0
+        pos = 0
+        neg = 0
         for i in range(0, len(nums), 2):
-            newArr[i + 1] = nums[j]
-            newArr[i] = nums[mid + j]
-            j += 1
+            while pos < len(nums) and nums[pos] < 0:
+                pos += 1
+            while neg < len(nums) and nums[neg] > 0:
+                neg += 1
+            newArr[i + 1] = nums[neg]
+            newArr[i] = nums[pos]
+            pos += 1
+            neg += 1
 
         return newArr

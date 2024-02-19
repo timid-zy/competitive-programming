944e8568c47587d7472f8981e2859a70ce996040
Nathan M.S <113636678+timid-angel@users.noreply.github.com>
2024-02-19 11:42:10 +0300

    Add solution for Next Greater Element II

--- src/leetcode/leetcode/next-greater-element-ii.py
+++ src/leetcode/leetcode/next-greater-element-ii.py
@@ -2,14 +2,9 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         
         ret_arr = [-1] * len(nums)
-        max_num = max(nums)
         dec_stack = []
         for i in range(len(nums) * 2):
             i = i % len(nums)
-            if len(dec_stack) == 0:
-                dec_stack.append((nums[i], i))
-                continue
-            
             while len(dec_stack) > 0 and dec_stack[-1][0] < nums[i]:
                 val, idx = dec_stack.pop()
                 ret_arr[idx] = nums[i]

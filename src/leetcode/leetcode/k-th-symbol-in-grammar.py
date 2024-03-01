176d4bfaa27c81b38d88a5a64b54d0409b070f40
Nathan M.S <113636678+timid-angel@users.noreply.github.com>
2024-03-01 13:24:23 +0300

    Add solution for K-th Symbol in Grammar

--- src/leetcode/leetcode/k-th-symbol-in-grammar.py
+++ src/leetcode/leetcode/k-th-symbol-in-grammar.py
@@ -1,10 +1,17 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        if n == 1:
-            return 0 if k == 1 else 1
-        midPoint = pow(2, n-1 )
-        if k <= midPoint:
-            return self.kthGrammar(n-1, k)
-        else:
-            itemOnLeftSide = self.kthGrammar(n-1, k-midPoint)
-            return 0 if itemOnLeftSide == 1 else 1
+        
+        def rec(n, k):
+            if n == 1:
+                return 0 if k == 1 else 1
+            
+            mid = pow(2, n - 1)
+            
+            if k <= mid:
+                return rec(n - 1, k)
+            else:
+                left = rec(n - 1, k - mid)
+                return 0 if left == 1 else 1
+
+
+        return rec(n, k) 

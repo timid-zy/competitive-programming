028960ea6d503ae9b3ddad011d1fca03d7027175
Nathan M.S <113636678+timid-angel@users.noreply.github.com>
2024-03-19 14:03:34 +0300

    Add solution for Interesting drink

--- src/leetcode/codeforces/706B Interesting-drink.py
+++ src/leetcode/codeforces/706B Interesting-drink.py
@@ -5,4 +5,4 @@ arr = list(map(int, input().split()))
 arr.sort()
 
 for i in range(int(input())):
-    print(bisect.bisect_right(arr, int(input())))
+    print(bisect.bisect_right(arr, int(input()))) 

d8200c575936bb7c952af33031e7325269c8bef5
Nathan Mesfin <113636678+timid-angel@users.noreply.github.com>
2024-09-04 18:23:13 +0300

    Created 'Floor and Mod' by A2SV Hub

---
+++ 04-Sep-2024/Floor and Mod 211024.py
@@ -0,0 +1,26 @@
+# Problem: Floor and Mod - https://codeforces.com/problemset/problem/1485/C
+
+import math
+
+def solve():
+    x, y = map(int, input().split())
+    res = 0
+    nxt_start, skip = 3, 5 
+
+    for rem in range(1, math.floor(x**0.5) + 1):
+        pairs = min(
+            (x - nxt_start) // rem + 1,  # check if x is a limiter
+            y - rem  # all possible
+        )
+
+        if pairs > 0:
+            res += pairs
+        
+        nxt_start += skip
+        skip += 2
+        
+    return res
+
+
+for _ in range(int(input())):
+    print(solve())

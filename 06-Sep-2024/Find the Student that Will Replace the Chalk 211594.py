ed8da1b005f5568391ed5bd7e3777fba4ed4b061
Nathan Mesfin <113636678+timid-angel@users.noreply.github.com>
2024-09-06 15:05:37 +0300

    Created 'Find the Student that Will Replace the Chalk' by A2SV Hub

---
+++ 06-Sep-2024/Find the Student that Will Replace the Chalk 211594.py
@@ -0,0 +1,8 @@
+# Problem: Find the Student that Will Replace the Chalk - https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/description/
+
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        k %= sum(chalk)
+        for i in range(len(chalk)):
+            k -= chalk[i]
+            if k < 0: return i

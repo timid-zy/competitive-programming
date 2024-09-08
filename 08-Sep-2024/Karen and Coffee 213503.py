1916f94ad85ed35267c8863d4d67f4033b889458
Nathan Mesfin <113636678+timid-angel@users.noreply.github.com>
2024-09-08 12:52:09 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 08-Sep-2024/Karen and Coffee 213503.py
@@ -0,0 +1,26 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+N, K, M = map(int, input().split())
+Q = [0] * (2 * (10 ** 5) + 1)
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    Q[a-1] -= 1
+    Q[b] += 1
+
+r_sum = 0
+for i in range(len(Q) - 1, -1, -1):
+    r_sum += Q[i]
+    Q[i] = r_sum
+
+admissable = [0] * len(Q)
+r_sum = 0
+for i in range(len(Q)):
+    if Q[i] >= K:
+        r_sum += 1
+    
+    admissable[i] = r_sum
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    print(admissable[b] - admissable[a-1])

cc574045627f421a52b0a6575e453924ad6167fa
Nathan Mesfin <113636678+timid-angel@users.noreply.github.com>
2024-09-15 16:04:11 +0300

    Created 'E - Word Transformation' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 15-Sep-2024/E - Word Transformation 215278.py
@@ -0,0 +1,23 @@
+# Problem: E - Word Transformation - https://codeforces.com/gym/543431/problem/E
+
+from collections import Counter
+
+def solve():
+    S, T = input().split()
+    counter = Counter(T)
+    ti = len(T) - 1
+    for i in range(len(S) - 1, -1, -1):
+        if S[i] == T[ti]:
+            counter[S[i]] -= 1
+            ti -= 1
+        
+        elif counter[S[i]] > 0:
+            break
+
+        if ti == -1:
+            break
+    
+    return "YES" if ti == -1 else "NO"
+
+for _ in range(int(input())):
+    print(solve())

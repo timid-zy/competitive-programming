8e3e36612815f685f6cdcac8a255052791502a97
Nathan M.S <natms24@gmail.com>
2024-09-26 13:12:36 +0300

    Created 'My Calendar I'

---
+++ 26-Sep-2024/My Calendar I.py
@@ -0,0 +1,18 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.arr = []
+        
+
+    def book(self, start: int, end: int) -> bool:
+        for x, y in self.arr:
+            if x <= start < y or x < end <= y or (start <= x and y <= end):
+                return False
+
+        self.arr.append((start, end))
+        return True      
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)

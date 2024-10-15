363cd1cb7b6d864d816304cf7219d986aa78a65c
Nathan Mesfin <113636678+timid-angel@users.noreply.github.com>
2024-10-15 13:18:10 +0300

    Created 'The Two Routes' by A2SV Hub

---
+++ 15-Oct-2024/The Two Routes 220123.py
@@ -0,0 +1,40 @@
+# Problem: The Two Routes - https://codeforces.com/problemset/problem/601/A
+
+from collections import defaultdict, deque
+
+def solve():
+    N, M = map(int, input().split())
+    graph = defaultdict(set)
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].add(b)
+        graph[b].add(a)
+
+    is_rail = False
+    if N in graph[1]:
+        is_rail = True
+
+    queue = deque([1])
+    seen = set([1])
+    lvl = 0
+
+    while queue:
+        for _ in range(len(queue)):
+            c = queue.popleft()
+            if c == N:
+                print(lvl)
+                return
+
+            for nb in range(1, N+1):
+                if (is_rail and nb in graph[c]) or (not is_rail and nb not in graph[c]):
+                    continue
+                
+                if nb not in seen:
+                    seen.add(nb)
+                    queue.append(nb)
+        
+        lvl += 1
+
+    print(-1)
+
+solve()

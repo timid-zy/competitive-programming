83f700695f925e7794b756b942e62e4ff9e4b859
Nathan Mesfin <113636678+timid-angel@users.noreply.github.com>
2024-12-08 14:31:23 +0300

    Created 'Twin Permutations' by A2SV Hub

---
+++ 08-Dec-2024/Twin Permutations 224650.py
@@ -0,0 +1,15 @@
+# Problem: Twin Permutations - https://codeforces.com/contest/1831/problem/A
+
+def solve():
+    n  = int(input())
+    arr = list(map(int, input().split()))
+    
+    target = n + 1
+    for i in range(len(arr)):
+        arr[i] = target - arr[i]
+    
+    return arr
+
+for _ in range(int(input())):
+    res = solve()
+    print(*res)

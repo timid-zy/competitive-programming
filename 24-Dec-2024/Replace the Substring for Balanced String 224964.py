146ed5d3de4d0addd17b99f230316165d1bbee31
Nathan M. <113636678+timid-angel@users.noreply.github.com>
2024-12-24 23:36:34 +0300

    Created 'Replace the Substring for Balanced String' by A2SV Hub

---
+++ 24-Dec-2024/Replace the Substring for Balanced String 224964.py
@@ -0,0 +1,27 @@
+# Problem: Replace the Substring for Balanced String - https://leetcode.com/problems/replace-the-substring-for-balanced-string/
+
+class Solution:
+    def balancedString(self, s: str) -> int:
+        n = len(s) // 4
+        req = Counter({'Q': n, 'W': n, 'E': n, 'R': n})
+        req = dict(Counter(s) - req)
+        if len(req) == 0: return 0
+        curr = {'Q': 0, 'W': 0, 'E': 0, 'R': 0}
+        start = 0
+        min_len = len(s)
+        for i in range(len(s)):
+            if s[i] not in req: continue
+            curr[s[i]] += 1
+            while start < len(s) and (s[start] not in req or curr[s[start]] > req[s[start]]):
+                curr[s[start]] -= 1
+                start += 1
+            
+            valid = True
+            for key in req:
+                if curr[key] < req[key]:
+                    valid = False
+                    break
+            if valid: 
+                min_len = min(min_len, i - start + 1)
+
+        return min_len

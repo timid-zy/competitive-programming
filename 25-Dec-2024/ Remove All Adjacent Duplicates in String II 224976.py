995ffd35f98c3a80ee5f3a306572638a00d0de29
Nathan M. <113636678+timid-angel@users.noreply.github.com>
2024-12-25 13:40:44 +0300

    Created ' Remove All Adjacent Duplicates in String II' by A2SV Hub

---
+++ 25-Dec-2024/ Remove All Adjacent Duplicates in String II 224976.py
@@ -0,0 +1,19 @@
+# Problem:  Remove All Adjacent Duplicates in String II - https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/description/
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stk = []
+        for c in s:
+            if stk and stk[-1][0] == c:
+                stk[-1][1] += 1
+            else:
+                stk.append([c, 1])
+            
+            if stk[-1][1] == k:
+                stk.pop()
+        
+        res = ""
+        for c, count in stk:
+            res += c * count
+        
+        return res
